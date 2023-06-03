413eee10bf0315ef9cf9b9cc3cd4af99b9e5b45c
Its-Saif <saif41428@gmail.com>
2023-06-03 10:20:58 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// // Get the game element
+// const gameElement = document.querySelector(".game");
+
+// // Start the game animation after a delay
+// setTimeout(() => {
+// 	gameElement.classList.add("animate");
+// }, 10000); // Delay in milliseconds (3s)
