d97bb2d094020a3e6edf62d76f884d02712b4bd3
Its-Saif <saif41428@gmail.com>
2023-06-03 11:05:53 +0530

    Fixed the Out of bound bug

--- index.js
+++
@@ -1,7 +0,0 @@
-// // Get the game element
-// const gameElement = document.querySelector(".game");
-
-// // Start the game animation after a delay
-// setTimeout(() => {
-// 	gameElement.classList.add("animate");
-// }, 10000); // Delay in milliseconds (3s)
